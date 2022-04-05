9b0052fb5669d79b43d13a61192eea50db78dc33
ValEmpire <palma_arcival@yahoo.com>
2022-04-05 18:17:46 -0400

    add insurance package action

---
+++ src/redux/actions/insurancePackage.action.js
@@ -0,0 +1,21 @@
+import { SET_INSURANCE_PACKAGES } from "../../const";
+
+/**
+ * * This action will get all the insurance packages and dispatch to reducers with type SET_INSURANCE_PACKAGES
+ * Todo: use axios when getting data from backend
+ */
+export const setInsurancePackages = () => async (dispatch) => {
+  try {
+    // fake data
+    const res = [];
+
+    dispatch({
+      type: SET_INSURANCE_PACKAGES,
+      payload: res,
+    });
+
+    return;
+  } catch (err) {
+    console.log(err);
+  }
+};
